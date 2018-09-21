284cb0a31758b03857886e4579b6e0941f8cb058
RyanGoslingsBugle <repairist@gmail.com>
2018-09-21 06:38:28 -0400

    Bump rails version to 4.2.7.1

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
   root 'pages#index'
-  get '/about' => 'pages#about'
+  get 'about' => 'pages#about'
 end
